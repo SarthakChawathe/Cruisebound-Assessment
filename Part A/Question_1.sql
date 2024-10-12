bcebf174e17ede34426bd5b6ac2d8591d3be1ac6
Sarthak Chawathe <sarthak.chawathe2012@gmail.com>
2024-10-12 17:46:13 -0400

    modified DATEDIFF()

--- Part A/Question_1.sql
+++ Part A/Question_1.sql
@@ -6,5 +6,5 @@ SELECT id,
     end_date,
     DATEDIFF(end_date, start_date) AS duration_days
 FROM sailing
-WHERE DATEDIFF(end_date, start_date) > 7
+WHERE DATEDIFF(day, end_date, start_date) > 7
     AND start_date > CURRENT_DATE;
